235843840778aab06bf229d3c60883a622da2501
Kjartan Kristjansson <kjarisk@gmail.com>
2020-06-23 14:29:58 +0200

    Kjartan update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kjartan er ad læra
         </p>
         <a
           className="App-link"
